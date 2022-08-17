c09dc5f3a983fcdf51ee6094c1d9eb5659d805c4
harou24 <haroutioun24@gmail.com>
2022-08-17 22:47:22 +0200

    exemple one

--- main.go
+++ main.go
@@ -4,14 +4,28 @@ import("fmt"
         "time"
 )
 
-func main(){
-    go func(){
-        fmt.Println("Helllo")
-    }()
+func runFunnyTest(){
     for i:= 0; i<=9; i++{
         go func(){
             fmt.Println(i)
         }()
     }
+}
+
+func runExpectedTest(){
+    for i:= 0; i<=9; i++{
+        i := i
+        go func(){
+            fmt.Println(i)
+        }()
+    }
+}
+
+func main(){
+    fmt.Println("Running funny test...")
+    runFunnyTest()
+    time.Sleep(time.Second)
+    fmt.Println("Running expected test...")
+    runExpectedTest()
     time.Sleep(time.Second)
 }
